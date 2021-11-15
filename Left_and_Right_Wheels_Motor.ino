00e3826a0f182c4d1f06f94f0f11ba12d8e10bd2
Pragash-Mohanarajah <77297468+Pragash-Mohanarajah@users.noreply.github.com>
2021-11-15 08:11:29 +0000

    Add Left and Right Wheels Motor File

---
+++ Left_and_Right_Wheels_Motor.ino
@@ -0,0 +1,59 @@
+/* Pragash Mohanarajah (C) November 2021. */
+
+/*
+This is a test sketch for the Adafruit assembled Motor Shield for Arduino v2
+It won't work with v1.x motor shields! Only for the v2's with built in PWM
+control
+
+For use with the Adafruit Motor Shield v2
+---->  http://www.adafruit.com/products/1438
+*/
+
+#include <Adafruit_MotorShield.h>
+
+// Create the motor shield object with the default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+
+// Select and configure port M1
+Adafruit_DCMotor *leftMotor = AFMS.getMotor(1);
+// Select and configure port M2
+Adafruit_DCMotor *rightMotor = AFMS.getMotor(2);
+
+// Create Global variables
+bool accel = true;
+
+void setup() {
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.println("Adafruit Motor Shield v2.3 Test for DC Motors M1 & M2");
+
+  if (!AFMS.begin()) {         // create with the default frequency 1.6KHz
+    Serial.println("Could not find Motor Shield. Check wiring.");
+    while (1);
+  }
+  Serial.println("Motor Shield found.");
+
+  // Set a speed of 150 to start, where 0 is off & 255 is max speed
+  leftMotor->setSpeed(150);
+  rightMotor->setSpeed(150);
+  // Turn on motors
+  leftMotor->run(FORWARD);
+  rightMotor->run(FORWARD);
+  // Turn off motors
+  leftMotor->run(RELEASE);
+  rightMotor->run(RELEASE);
+}
+
+void loop() {
+  uint8_t i;
+
+  if (accel) {
+    leftMotor->run(FORWARD);
+    rightMotor->run(FORWARD);
+    for (i=0; i<255; i++) {
+      leftMotor->setSpeed(i);
+      rightMotor->setSpeed(i);
+      delay(10);
+    }
+    accel = !accel;
+  }
+}
