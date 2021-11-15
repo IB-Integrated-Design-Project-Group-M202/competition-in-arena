c043369cc9620b9a01e6d4a2eff7e25e2f17a6dc
Pragash-Mohanarajah <77297468+Pragash-Mohanarajah@users.noreply.github.com>
2021-11-15 23:05:10 +0000

    Update Search_and_Identify_Dummy.ino

--- Search_and_Identify_Dummy.ino
+++ Search_and_Identify_Dummy.ino
@@ -1,1 +1,15 @@
+#define R_PIN A0
+#define PT1_PIN A1
+#define PT2_PIN A2
 
+void setup() {
+  // Configure two phototransistors as analog inputs
+  pinMode(PT1_PIN, INPUT);
+  pinMode(PT2_PIN, INPUT);
+  // Configure IR receiver as an analog input_pullup, i.e. with inverted logic with respect to 5V V_CC.
+  pinMode(R_PIN, INPUT_PULLUP);
+}
+
+void loop() {
+  
+}
