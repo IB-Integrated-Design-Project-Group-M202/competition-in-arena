329c133b90bb21300b30aaf3e84151a39a0ae5ff
Pragash-Mohanarajah <77297468+Pragash-Mohanarajah@users.noreply.github.com>
2021-11-16 15:01:28 +0000

    Update Left_and_Right_Wheels_Motor.ino

--- Left_and_Right_Wheels_Motor.ino
+++ Left_and_Right_Wheels_Motor.ino
@@ -23,7 +23,11 @@ Adafruit_DCMotor *rightMotor = AFMS.getMotor(2);
 
 
 // Create Global variables
-bool accel = true;
+bool accel = true, decel = false;
+unsigned long previousMillis = 0;
+long duration;
+const int ledPin = 8, echoPin = 2, trigPin = 3;
+int distance = 0, ledState = LOW;
 
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
@@ -44,17 +48,61 @@ void setup() {
   // Turn off motors
   leftMotor->run(RELEASE);
   rightMotor->run(RELEASE);
+  // Configure LED pin as an output
+  pinMode(ledPin, OUTPUT);
+  // Configure Trigger pin of HC-SR04 as an output
+  pinMode(trigPin, OUTPUT);
+  // Configure Echo pin of HC-SR04 as an input
+  pinMode(echoPin, INPUT);
+  // Initialise all output pins as LOW
+  digitalWrite(ledPin, LOW);
+  digitalWrite(trigPin, LOW);
 }
 
 void loop() {
-  if (accel) {
-    leftMotor->run(FORWARD);
-    rightMotor->run(FORWARD);
-    for (uint8_t i=0; i<255; i++) {
-      leftMotor->setSpeed(i);
-      rightMotor->setSpeed(i);
-      delay(10);
+  unsigned long currentMillis = millis();
+  unsigned long time_elapsed = currentMillis - previousMillis;
+  if ((time_elapsed <= 5) && (distance == 0)) {
+    digitalWrite(trigPin, HIGH);
+    delay(10);
+    digitalWrite(trigPin, LOW);
+    duration = pulseIn(echoPin, HIGH);
+    distance = duration * 3.4 / 20;
+    if (distance < 150) decel = true;
+  }
+  if (time_elapsed >= 250) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledState == LOW) {
+      ledState = HIGH;
+    } else {
+      ledState = LOW;
     }
-    accel = !accel;
+
+    // set the LED with the ledState of the variable:
+    digitalWrite(ledPin, ledState);
+    distance = 0;
+  }
+  if ((time_elapsed % 10) == 0) {
+    if (accel) {
+      leftMotor->run(FORWARD);
+      rightMotor->run(FORWARD);
+      for (uint8_t i=0; i<255; i++) {
+        leftMotor->setSpeed(i);
+        rightMotor->setSpeed(i);
+      }
+      accel = !accel;
+    }
+    if (decel) {
+      leftMotor->run(FORWARD);
+      rightMotor->run(FORWARD);
+      for (uint8_t i=0; i<255; i--) {
+        leftMotor->setSpeed(i);
+        rightMotor->setSpeed(i);
+      }
+      decel = !decel;
+    }
   }
 }
