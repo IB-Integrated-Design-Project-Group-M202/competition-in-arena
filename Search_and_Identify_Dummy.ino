30d6f058f2365226e183614dfe02cfa91329a526
Pragash-Mohanarajah <77297468+Pragash-Mohanarajah@users.noreply.github.com>
2021-11-18 09:37:47 +0000

    Update Search_and_Identify_Dummy.ino

--- Search_and_Identify_Dummy.ino
+++ Search_and_Identify_Dummy.ino
@@ -1,6 +1,37 @@
+/* Pragash Mohanarajah (C) November 2021. */
+
+#include <Adafruit_MotorShield.h>
+
+// Create the motor shield object with the default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+
+// Select and configure port M1
+Adafruit_DCMotor *leftMotor = AFMS.getMotor(1);
+// Select and configure port M2
+Adafruit_DCMotor *rightMotor = AFMS.getMotor(2);
+
 const int r_Pin = A0, pt1_Pin = A1, pt2_Pin = A2, greenLED_Pin = 12, redLED_Pin = 13;
 
 void setup() {
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.println(F("Adafruit Motor Shield v2.3 Test for DC Motors M1 & M2"));
+
+  if (!AFMS.begin()) {         // create with the default frequency 1.6KHz
+    Serial.println(F("Could not find Motor Shield. Check wiring."));
+    while (1);
+  }
+  Serial.println(F("Motor Shield found."));
+
+  // Set a speed of 150 to start, where 0 is off & 255 is max speed
+  leftMotor->setSpeed(150);
+  rightMotor->setSpeed(150);
+  // Turn on motors
+  leftMotor->run(FORWARD);
+  rightMotor->run(FORWARD);
+  // Turn off motors
+  leftMotor->run(RELEASE);
+  rightMotor->run(RELEASE);
+  
   // Configure two phototransistors as analog inputs
   pinMode(pt1_Pin, INPUT);
   pinMode(pt2_Pin, INPUT);
@@ -12,5 +43,9 @@ void setup() {
 }
 
 void loop() {
-  
+  // Turn robot right slowly
+  leftMotor->setSpeed(50);
+  rightMotor->setSpeed(50);
+  leftMotor->run(FORWARD);
+  rightMotor->run(BACKWARD);
 }
