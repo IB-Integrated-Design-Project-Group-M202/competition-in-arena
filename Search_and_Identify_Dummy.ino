a7dd362f499e686f8faa1da5eda772f5bb244280
Pragash-Mohanarajah <77297468+Pragash-Mohanarajah@users.noreply.github.com>
2021-11-18 08:58:33 +0000

    Update Search_and_Identify_Dummy.ino

--- Search_and_Identify_Dummy.ino
+++ Search_and_Identify_Dummy.ino
@@ -1,13 +1,14 @@
-#define R_PIN A0
-#define PT1_PIN A1
-#define PT2_PIN A2
+const int r_Pin = A0, pt1_Pin = A1, pt2_Pin = A2, greenLED_Pin = 12, redLED_Pin = 13;
 
 void setup() {
   // Configure two phototransistors as analog inputs
-  pinMode(PT1_PIN, INPUT);
-  pinMode(PT2_PIN, INPUT);
+  pinMode(pt1_Pin, INPUT);
+  pinMode(pt2_Pin, INPUT);
   // Configure IR receiver as an analog input_pullup, i.e. with inverted logic with respect to 5V V_CC.
-  pinMode(R_PIN, INPUT_PULLUP);
+  pinMode(r_Pin, INPUT_PULLUP);
+  // Configure green and red indication LEDs as analog outputs
+  pinMode(greenLED_Pin, OUTPUT);
+  pinMode(redLED_Pin, OUTPUT);
 }
 
 void loop() {
