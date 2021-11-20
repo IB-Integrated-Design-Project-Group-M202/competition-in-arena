cac043aab5fe63c9c61dfc6ace641fb6eb9ddfdf
Pragash-Mohanarajah <77297468+Pragash-Mohanarajah@users.noreply.github.com>
2021-11-20 13:08:26 +0000

    Update Search_and_Identify_Dummy.ino

--- Search_and_Identify_Dummy.ino
+++ Search_and_Identify_Dummy.ino
@@ -12,12 +12,12 @@ Adafruit_DCMotor *leftMotor = AFMS.getMotor(1);
 Adafruit_DCMotor *rightMotor = AFMS.getMotor(2);
 
 bool gyro_calibrated = false, search_area = false, aligned = false, arrived = false, identified = false;
-unsigned long startMillis = 0, currentMillis = 0, currentMicros = 0, lastMicros = 0, elapsedMicros = 0, elapsedMillis = 0;
-const int r_Pin = A0, pt1_Pin = A1, pt2_Pin = A2, greenLED_Pin = 12, redLED_Pin = 13, indicatorDelay = 5000, numReadings = 115;
+unsigned long startMillis = 0, currentMillis = 0, elapsedMillis = 0, gyroMillis = 0;
+const int echoPin = 2, trigPin = 3, r_Pin = A0, pt1_Pin = A1, pt2_Pin = A2, amberLED_Pin = 8, greenLED_Pin = 12, redLED_Pin = 13, indicatorDelay = 5000, numReadings = 115;
 int pt1_readings[numReadings], pt2_readings[numReadings], readIndex = 0, pt1_total = 0, pt2_total = 0, pt1_average = 0, pt2_average = 0, pt1_Min = 1023, pt2_Min = 1023, pt1_Max = 0, pt2_Max = 0;
-int i, j;
+int i, j, amberLED_State = LOW;
 float x, y, z, angle_total = 0, angle_offset = 0, angle_turned = 0, pt1_angle = 0, pt2_angle = 0, dummy_angle = 0;
-uint8_t dummy = 0;
+uint8_t dummy = 0, amberLED_duration = 250, trigger_duration = 10;
 
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
@@ -55,6 +55,10 @@ void setup() {
   pinMode(pt2_Pin, INPUT);
   // Configure IR receiver as an analog input_pullup, i.e. with inverted logic with respect to 5V V_CC.
   pinMode(r_Pin, INPUT_PULLUP);
+  // Configure Trigger pin of HC-SR04 as an output
+  pinMode(trigPin, OUTPUT);
+  // Configure Echo pin of HC-SR04 as an input
+  pinMode(echoPin, INPUT);
   // Configure green and red indication LEDs as analog outputs
   pinMode(greenLED_Pin, OUTPUT);
   pinMode(redLED_Pin, OUTPUT);
@@ -75,16 +79,14 @@ void calibrate_gyro() {
 }
 
 void pt_reading() {
-  pt1_total -= pt1_readings[readIndex]; pt2_total -= pt2_readings[readIndex];
+  pt1_total = pt1_total - pt1_readings[readIndex]; pt2_total = pt2_total - pt2_readings[readIndex];
   pt1_readings[readIndex] = analogRead(pt1_Pin); pt2_readings[readIndex] = analogRead(pt2_Pin);
-  if (pt1_readings[readIndex] < pt1_Min && pt1_readings[readIndex] > 20) pt1_Min = pt1_readings[readIndex];
-  if (pt1_readings[readIndex] > pt1_Max && pt1_readings[readIndex] < 1000) pt1_Max = pt1_readings[readIndex]; else i += 1;
-  if (pt2_readings[readIndex] < pt2_Min && pt2_readings[readIndex] > 20) pt2_Min = pt2_readings[readIndex];
-  if (pt2_readings[readIndex] > pt2_Max && pt2_readings[readIndex] < 1000) pt2_Max = pt2_readings[readIndex]; else j += 1;
-  pt1_total += pt1_readings[readIndex]; pt2_total += pt2_readings[readIndex];
+  pt1_total = pt1-total + pt1_readings[readIndex]; pt2_total = pt2_total + pt2_readings[readIndex];
   readIndex += 1;
   if (readIndex >= numReadings) {
     readIndex = 0;
+    if (pt1_total / numReadings < pt1_average) i += 1;
+    if (pt2_total / numReadings < pt2_average) j += 1;
     pt1_average = pt1_total / numReadings; pt2_average = pt2_total / numReadings;
     delay(12);
   }
@@ -98,20 +100,31 @@ void align_with_dummy() {
     leftMotor->run(BACKWARD); rightMotor->run(FORWARD);
   } else {
     if (abs(angle_turned - dummy_angle) <= 0.5) {
-      leftMotor->run(RELEASE); rightMotor->run(RELEASE);
+      leftMotor->run(RELEASE); rightMotor->run(RELEASE); aligned = true;
     } else { leftMotor->run(FORWARD); rightMotor->run(BACKWARD); }
   }
-  if (IMU.gyroscopeAvailable()) IMU.readGyroscope(x, y, z);
-  currentMicros = micros();
-  elapsedMicros = currentMicros - lastMicros;
-  if (elapsedMicros >= 50000) angle_turned += (z - angle_offset) * elapsedMicros/1E6 * 180/160.7;
   if (i >= 3) pt1_angle = angle_turned; if (j >= 3) pt2_angle = angle_turned;
   if (pt1_angle == 0 || pt2_angle == 0) pt_reading(); else dummy_angle = (pt1_angle + pt2_angle) / 2;
-  lastMicros = currentMicros;
 }
 
 void drive_to_dummy() {
-  
+  if (elapsedMillis >= amberLED_duration) {
+    // save the last time you blinked the LED
+    amberLED_Millis = currentMillis;
+
+    // if the LED is off turn it on and vice-versa:
+    if (amberLED_State == LOW) amberLED_State = HIGH; else amberLED_State = LOW;
+
+    // set the LED with the ledState of the variable:
+    digitalWrite(amberLED_Pin, amberLED_State);
+    
+    digitalWrite(trigPin, HIGH);
+    delay(trigger_duration);
+    digitalWrite(trigPin, LOW);
+    echo_duration = pulseIn(echoPin, HIGH);
+    distance = echo_duration * 3.4 / 20;
+    if (distance < 150 && search_area) { arrived = true; leftSpeed = 0; rightSpeed = 0; }
+  }
 }
 
 void identify_dummy() {
@@ -121,8 +134,12 @@ void identify_dummy() {
 void loop() {
   currentMillis = millis();
   elapsedMillis = currentMillis - startMillis;
+  gyroMillis = currentMillis - lastMillis;
   if (IMU.accelerationAvailable()) IMU.readAcceleration(x, y, z);
   if (x > 0.28) search_area = true;
+  if (IMU.gyroscopeAvailable()) IMU.readGyroscope(x, y, z);
+  if (gyroMillis >= 50) angle_turned += (z - angle_offset) * gyroMillis/1E3 * 180/160.7;
+  lastMillis = currentMillis;
   if (!gyro_calibrated && !aligned) calibrate_gyro();
   if (gyro_calibrated && !aligned) align_with_dummy();
   if (gyro_calibrated && aligned && !arrived) drive_to_dummy();
