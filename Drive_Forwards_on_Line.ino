b755c56b70df4b70d6fb02b1a6dd666b9236da10
Pragash-Mohanarajah <77297468+Pragash-Mohanarajah@users.noreply.github.com>
2021-11-22 14:13:58 +0000

    Update Drive_Forwards_on_Line.ino

--- Drive_Forwards_on_Line.ino
+++ Drive_Forwards_on_Line.ino
@@ -14,7 +14,7 @@ Adafruit_DCMotor *rightMotor = AFMS.getMotor(2);
 
 
 // Create Global variables
-bool accel = true, decel = false;
+bool accel = true, decel = false, on_line = true;
 const int leftLineSensor = 4, rightLineSensor = 7;
 int leftSensorStatus = LOW, rightSensorStatus = LOW;
 uint8_t leftSpeed = 0, rightSpeed = 0;
@@ -39,6 +39,9 @@ void setup() {
   leftMotor->run(RELEASE);
   rightMotor->run(RELEASE);
   
+  // Configure Left and Right Line Sensors as inputs
+  pinMode(leftLineSensor, INPUT);
+  pinMode(rightLineSensor, INPUT);
 }
 
 void loop() {
@@ -46,34 +49,32 @@ void loop() {
   rightSensorStatus = digitalRead(rightLineSensor);
   leftMotor->setSpeed(leftSpeed);
   rightMotor->setSpeed(rightSpeed);
+  leftMotor->run(FORWARD);
+  rightMotor->run(FORWARD);
   
   if (leftSensorStatus == LOW && rightSensorStatus == LOW) {
+    on_line = true;
     if (leftSpeed != rightSpeed) {
       leftSpeed = (leftSpeed + rightSpeed) / 2;
       rightSpeed = (leftSpeed + rightSpeed) / 2;
     } else
-    if (leftSpeed == rightSpeed) if (leftSpeed < 255 || rightSpeed < 255) if (!decel) accel = true;
+    if (leftSpeed == rightSpeed && !decel) { leftSpeed = 255; rightSpeed = 255; }
   } else
-  if (leftSensorStatus == HIGH && rightSensorStatus == LOW) {
-    leftSpeed += 51; rightSpeed -= 51;
+  if (leftSensorStatus == HIGH || rightSensorStatus == HIGH) on_line = false;
+  if (!on_line && leftSensorStatus == HIGH && rightSensorStatus == LOW) {
+    leftSpeed += 51; rightSpeed -= 51; delay(50);
   } else
-  if (leftSensorStatus == LOW && rightSensorStatus == HIGH) {
-    leftSpeed -= 51; rightSpeed += 51;
+  if (!on_line && leftSensorStatus == LOW && rightSensorStatus == HIGH) {
+    leftSpeed -= 51; rightSpeed += 51; delay(50);
   }
   if (accel) {
     if (leftSpeed < 255) leftSpeed += 51;
     if (rightSpeed < 255) rightSpeed += 51;
-    if (leftSpeed == 255 && rightSpeed == 255) accel = !accel;
-    leftMotor->run(FORWARD);
-    rightMotor->run(FORWARD);
-    delay(10);
+    if (leftSpeed == 255 && rightSpeed == 255) accel = false;
   }
   if (decel) {
     if (leftSpeed > 0) leftSpeed -= 51;
     if (rightSpeed > 0) rightSpeed -= 51;
-    if (leftSpeed == 0 && rightSpeed == 0) decel = !decel;
-    leftMotor->run(FORWARD);
-    rightMotor->run(FORWARD);
-    delay(10);
+    if (leftSpeed == 0 && rightSpeed == 0) decel = false;
   }
 }
